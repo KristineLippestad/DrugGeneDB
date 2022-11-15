d57fd9820f5cf1e28648e406e2fd1863f5117082
KristineLippestad <krislipp@stud.ntnu.no>
2022-11-15 13:00:16 +0100

    Updated queries

--- queries.py
+++ queries.py
@@ -50,7 +50,6 @@ def drug(db_file, file_name, drugName):
     create_connection(db_file)
     cursor.execute("SELECT * FROM Drug WHERE drugName = ? and type = 'Small molecule'", (drugName, ))
     df = pd.DataFrame(cursor.fetchall(), columns=["drugID", "drugName", "type"])
-    print(df)
     drugPanel(file_name, df)
     con.close()
 
@@ -72,16 +71,36 @@ def drugPanel(fileName, df):
             targetList.clear()
     f.close()
 
-def targetProfile(db_file, file_name, id, limit):
+def targetProfile(db_file, file_name, id_list, kd_limit = None, ki_limit = None, ic50_limit = None):
     create_connection(db_file)
-    cursor.execute("SELECT DISTINCT Interaction.drugID, actionType, Interaction.HGNC FROM ((Interaction INNER JOIN MeasuredFor ON Interaction.DrugID = MeasuredFor.DrugID) INNER JOIN BindingAffinity ON MeasuredFor.BindingReactionID = BindingAffinity.BindingReactionID) WHERE Interaction.DrugID = ? and  Kd_max < ?", (id, limit))
-    df = pd.DataFrame(cursor.fetchall(), columns=["drugID", "actionType", "HGNC"])
-    drugPanelTP(file_name, df)
+    for i in id_list:
+        frames = []
+        if kd_limit != None:
+            cursor.execute("SELECT DISTINCT Interaction.drugID, actionType, Interaction.HGNC FROM ((Interaction INNER JOIN MeasuredFor ON Interaction.DrugID = MeasuredFor.DrugID) INNER JOIN BindingAffinity ON MeasuredFor.BindingReactionID = BindingAffinity.BindingReactionID) WHERE Interaction.DrugID = ? and  Kd_max < ?", (i, kd_limit))
+            df1 = pd.DataFrame(cursor.fetchall(), columns=["drugID", "actionType", "HGNC"])
+            frames.append(df1)
+
+        if ki_limit != None: 
+            cursor.execute("SELECT DISTINCT Interaction.drugID, actionType, Interaction.HGNC FROM ((Interaction INNER JOIN MeasuredFor ON Interaction.DrugID = MeasuredFor.DrugID) INNER JOIN BindingAffinity ON MeasuredFor.BindingReactionID = BindingAffinity.BindingReactionID) WHERE Interaction.DrugID = ? and  Ki_max < ?", (i, ki_limit))
+            df2 = pd.DataFrame(cursor.fetchall(), columns=["drugID", "actionType", "HGNC"])
+            frames.append(df2)
+
+        if ic50_limit: 
+            cursor.execute("SELECT DISTINCT Interaction.drugID, actionType, Interaction.HGNC FROM ((Interaction INNER JOIN MeasuredFor ON Interaction.DrugID = MeasuredFor.DrugID) INNER JOIN BindingAffinity ON MeasuredFor.BindingReactionID = BindingAffinity.BindingReactionID) WHERE Interaction.DrugID = ? and  Kd_max < ?", (i, ic50_limit))
+            df3 = pd.DataFrame(cursor.fetchall(), columns=["drugID", "actionType", "HGNC"])
+            frames.append(df3)
+            
+        df = pd.concat(frames, ignore_index=True)
+        drugPanelTP(file_name, df)
+        frames.clear()
     con.close()
 
 def drugPanelTP(fileName, df):
-    with open(fileName, 'w') as f:
-        f.write("#Name\tTarget" + "\n") # Retrieve all targets for a given compound.
+    with open(fileName, 'a+') as f:
+        f.seek(0)
+        first_line = f.readline().rstrip('\n')
+        if first_line != "#Name\tTarget":
+            f.write("#Name\tTarget" + "\n") # Retrieve all targets for a given compound.
         targetList = []
         try:
             for ind in df.index:
@@ -89,15 +108,14 @@ def drugPanelTP(fileName, df):
                 aType = df["actionType"][ind]
                 if aType == "Inhibitor":
                     aT = "inhibits"
-                print(df["HGNC"][ind])
+                df["HGNC"][ind]
                 targetList.append(df["HGNC"][ind])
-            f.write(drugId + "\t" + aT + "\t" + "\t".join(targetList) + "\n")
+            f.write(drugId + "\t" + aT + "\t" + "\t".join(set(targetList)) + "\n")
         except:
-            print("No interactions with binding affinity measured below the given limit value.") 
+            print("No interactions with binding affinity measured below the given limit value.")
     f.close()
-
 
 #smallMoleculeAndTargets('/Users/kristinelippestad/Dokumenter/Master/Test_DB.db', 'FLT4', 'Small molecule')
 #twoTargets('/Users/kristinelippestad/Dokumenter/Master/Test_DB.db', 'FLT4', 'CSF1R')
 #drug('/Users/kristinelippestad/Dokumenter/Master/Test_DB.db','drugTests.txt','SUNITINIB')
-targetProfile('/Users/kristinelippestad/Dokumenter/Master/DTP.db', '/Users/kristinelippestad/Dokumenter/Master/drugT4.txt', 'CHEMBL6', 10)
+targetProfile('/Users/kristinelippestad/Dokumenter/Master/DTP.db', '/Users/kristinelippestad/Dokumenter/Master/drugDrug.txt', ['CHEMBL98', 'CHEMBL1289494', 'CHEMBL1614710', 'CHEMBL1173655'], ki_limit = 20, ic50_limit = 30)
